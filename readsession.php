46bb97b74013638fdb4afaec629349433f83d244
russellcachia <84193098+russellcachia@users.noreply.github.com>
2021-05-14 17:53:10 +0200

    Update readsession.php

--- readsession.php
+++ readsession.php
@@ -1,10 +1,10 @@
-<?php
-    session_start();
-    if (isset($_SESSION['storename'])) {
-        echo "User Name:", $_SESSION['sotrename'];
-        echo "User age:", $_SESSION['storeage'];
-    } else {        
-        $_SESSION['username'] = $_POST["name"];
-        $_SESSION['storeage'] = $_POST["age"] ;
-    }
-?>
+5)  <?php
+     session_start();
+      if (isset($_SESSION['storename'])) {
+         echo "User Name:", $_SESSION['sotrename'];
+         echo "User age:", $_SESSION['storeage'];
+      } else {        
+          $_SESSION['username'] = $_POST["name"];
+         $_SESSION['storeage'] = $_POST["age"] ;
+       }
+    ?>
