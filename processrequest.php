84f5e775eaf245100dd74a6e7c4b1857dd204499
osamaswan <84193033+osamaswan@users.noreply.github.com>
2021-05-14 16:49:17 +0100

    Update processrequest.php

--- processrequest.php
+++ processrequest.php
@@ -1,6 +1,21 @@
-
-<form action="" method="post">
+<?php
+session_start();
+$showDate = date("Y.m.d");
+$_SESSION['storeDate'] = $showDate;
+$_SESSION['storename'] = $_POST["name"];
+$_SESSION['storeage'] = $_POST["age"];
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Test</title>
+    </head>
+    <body>
+        <form action="" method="post">
             <input type="text" name="$name" placeholder="Your name" required />
             <input type="number" name="$age" placeholder="Your age" required />
             <input type="submit" value="Submit" />
         </form>
+    </body>
+</html>
+?>
