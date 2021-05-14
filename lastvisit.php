9fddeb27e631db8ef706b591e84aed4a7b98528b
dimitar177 <83815561+dimitar177@users.noreply.github.com>
2021-05-14 17:44:59 +0200

    Update lastvisit.php

--- lastvisit.php
+++ lastvisit.php
@@ -1,1 +1,6 @@
 
+<form action="" method="post">
+            <input type="text" name="$name" placeholder="Your name" required />
+            <input type="number" name="$age" placeholder="Your age" required />
+            <input type="submit" value="Submit" />
+        </form>
