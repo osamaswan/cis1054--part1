9d61a579028dde48ea5a3fa012189deac520a2a9
osamaswan <84193033+osamaswan@users.noreply.github.com>
2021-06-16 17:25:45 +0100

    Update processrequest.php

--- processrequest.php
+++ processrequest.php
@@ -1,7 +1,7 @@
+//Get method
 4)  <?php
         session_start();
-        $showDate = date("Y.m.d");
-        $_SESSION['storeDate'] = $showDate;
+        $_SESSION['storeDate'] = date("Y.m.d");
         $_SESSION['storename'] = $_POST["name"];
         $_SESSION['storeage'] = $_POST["age"];
     ?>
@@ -12,8 +12,8 @@
           </head>
           <body>
                 <form action="" method="post">
-                   <input type="text" name="$name" placeholder="Your name" required />
-                  <input type="number" name="$age" placeholder="Your age" required />
+                   <input type="text" name="name" placeholder="Your name" required />
+                  <input type="number" name="age" placeholder="Your age" required />
                     <input type="submit" value="Submit" />
              </form>
             </body>
