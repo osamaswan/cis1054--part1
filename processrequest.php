c5d18f1753498e6419542563fcacc5871f46cfe6
osamaswan <84193033+osamaswan@users.noreply.github.com>
2021-06-16 17:39:56 +0100

    Update processrequest.php

--- processrequest.php
+++ processrequest.php
@@ -1,4 +1,4 @@
-//Get method
+//Post method
 4)  <?php
         session_start();
         $_SESSION['storeDate'] = date("Y.m.d");
@@ -12,6 +12,27 @@
           </head>
           <body>
                 <form action="readsession.php" method="POST">
+                   <input type="text" name="name" placeholder="Your name" required />
+                  <input type="number" name="age" placeholder="Your age" required />
+                    <input type="submit" value="Submit" />
+             </form>
+            </body>
+        </html>
+    ?>
+//Get
+<?php
+        session_start();
+        $_SESSION['storeDate'] = date("Y.m.d");
+        $_SESSION['storename'] = $_GET["name"];
+        $_SESSION['storeage'] = $_GET["age"];
+    ?>
+        <!DOCTYPE html>
+        <html>
+           <head>
+                <title>Test</title>
+          </head>
+          <body>
+                <form action="readsession.php" method="GET">
                    <input type="text" name="name" placeholder="Your name" required />
                   <input type="number" name="age" placeholder="Your age" required />
                     <input type="submit" value="Submit" />
