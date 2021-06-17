03480a6ef900f581ab82d1b387502e6a7c6ab766
osamaswan <84193033+osamaswan@users.noreply.github.com>
2021-06-17 14:16:12 +0100

    Update serverdt.php

--- serverdt.php
+++ serverdt.php
@@ -1,4 +1,15 @@
-2)	<?php
-  	  echo date("h:i:s Y/m/d")."\n”;
-?>
-
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Date and time</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+<body>
+    <p>
+    <?php
+        echo("the time and date is" date("h:i:s Y/m/d"). "\n");
+    ?>
+</body>
+</html>
